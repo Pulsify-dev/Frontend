080f1de206e9fa8c68a5ac820792520528b766f6
Ahmad Hisham Gamal Eldeen <ahmadhisham111@gmail.com>
2026-03-18 16:51:49 +0200

    added Authentication part

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,27 +1,35 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import { PulsifyPlaylistsView } from "./pages/PulsifyPlaylistsView";
-import { PulsifyPlaylistDetailView } from "./pages/PulsifyPlaylistDetailView";
-import { PulsifyTrackUploadScreen } from "./pages/PulsifyTrackUploadScreen";
-import { PulsifyPremiumUpgradePage } from "./pages/PulsifyPremiumUpgradePage";
-import ProfilePage from "./profile/pages/ProfilePage";
-import "./App.css";
-
-function App() {
+import MainLayout from "@/layouts/MainLayout";
+import Login from "@/pages/Login";
+import Register from "@/pages/Register";
+import ForgotPassword from "@/pages/ForgotPassword";
+import { PulsifyPlaylistsView } from "@/pages/PulsifyPlaylistsView";
+import { PulsifyPlaylistDetailView } from "@/pages/PulsifyPlaylistDetailView";
+import { PulsifyTrackUploadScreen } from "@/pages/PulsifyTrackUploadScreen";
+import { PulsifyPremiumUpgradePage } from "@/pages/PulsifyPremiumUpgradePage";
+import TrackPage from "@/pages/TrackPage";
+import ProfilePage from "@/profile/pages/ProfilePage";
+const AppRoutes = () => {
   return (
-    <div className="pulsify-app-container">
-      <Routes>
-        <Route path="/" element={<Navigate to="/playlists" replace />} />
-        <Route path="/playlists" element={<PulsifyPlaylistsView />} />
-        <Route
-          path="/playlists/:playlistId"
-          element={<PulsifyPlaylistDetailView />}
-        />
-        <Route path="/upload" element={<PulsifyTrackUploadScreen />} />
-        <Route path="/premium" element={<PulsifyPremiumUpgradePage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-      </Routes>
-    </div>
-  );
-}
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+      </Route>
 
-export default App;
+      <Route path="/playlists" element={<PulsifyPlaylistsView />} />
+      <Route
+        path="/playlists/:playlistId"
+        element={<PulsifyPlaylistDetailView />}
+      />
+      <Route path="/upload" element={<PulsifyTrackUploadScreen />} />
+      <Route path="/premium" element={<PulsifyPremiumUpgradePage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/trackpage" element={<TrackPage />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
