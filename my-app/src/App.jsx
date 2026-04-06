f2a20ee8cf4a1de9cdb78dcdd807440514534467
Ahmad Hisham Gamal Eldeen <ahmadhisham111@gmail.com>
2026-04-06 17:07:55 +0200

    Fixed routing in app.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -10,11 +10,6 @@ import { PulsifyPremiumUpgradePage } from "@/pages/PulsifyPremiumUpgradePage";
 import TrackPage from "@/pages/TrackPage";
 import ProfilePage from "@/profile/pages/ProfilePage";
 
-// Module 3 – Social Graph
-import FollowingPage from "@/social/pages/FollowingPage";
-import FollowersPage from "@/social/pages/FollowersPage";
-import BlockedUsersPage from "@/social/pages/BlockedUsersPage";
-
 import { NotificationProvider } from "./context/NotificationContext";
 import { PlayerProvider } from "./context/PlayerContext";
 import PulsifyPlayerBar from "./components/common/PulsifyPlayerBar";
@@ -22,6 +17,9 @@ import DiscoveryFeedPage from "./pages/DiscoveryFeedPage";
 import SearchHubPage from "./pages/SearchHubPage";
 import TrendingChartsPage from "./pages/TrendingChartsPage";
 
+import FollowingPage from "@/social/pages/FollowingPage";
+import FollowersPage from "@/social/pages/FollowersPage";
+import BlockedUsersPage from "@/social/pages/BlockedUsersPage";
 const AppRoutes = () => {
   return (
     <NotificationProvider>
@@ -29,44 +27,31 @@ const AppRoutes = () => {
         <>
           <Routes>
             <Route element={<MainLayout />}>
-            </Route>
-              {/* Auth */}
               <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
               <Route path="/forgot-password" element={<ForgotPassword />} />
+            </Route>
 
-              {/* Profile - Module 2 */}
-              <Route path="/profile" element={<ProfilePage />} />
-              <Route path="/profile/:userId" element={<ProfilePage />} />
+            <Route path="/playlists" element={<PulsifyPlaylistsView />} />
+            <Route
+              path="/playlists/:playlistId"
+              element={<PulsifyPlaylistDetailView />}
+            />
+            <Route path="/upload" element={<PulsifyTrackUploadScreen />} />
+            <Route path="/premium" element={<PulsifyPremiumUpgradePage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/trackpage" element={<TrackPage />} />
 
-              {/* Module 3 – Social Graph */}
-              <Route path="/following" element={<FollowingPage />} />
-              <Route path="/following/:userId" element={<FollowingPage />} />
-              <Route path="/followers" element={<FollowersPage />} />
-              <Route path="/followers/:userId" element={<FollowersPage />} />
-              <Route path="/blocked" element={<BlockedUsersPage />} />
-
-              {/* Playlists - Module 7 */}
-              <Route path="/playlists" element={<PulsifyPlaylistsView />} />
-              <Route
-                path="/playlists/:playlistId"
-                element={<PulsifyPlaylistDetailView />}
-              />
-
-              {/* Upload - Module 4 */}
-              <Route path="/upload" element={<PulsifyTrackUploadScreen />} />
-
-              {/* Track - Module 4/5 */}
-              <Route path="/trackpage" element={<TrackPage />} />
-
-              {/* Premium - Module 12 */}
-              <Route path="/premium" element={<PulsifyPremiumUpgradePage />} />
-
-              {/* Discovery / Notifications - Module 8 */}
-              <Route path="/feed" element={<DiscoveryFeedPage />} />
-              <Route path="/search" element={<SearchHubPage />} />
-              <Route path="/trending" element={<TrendingChartsPage />} />
+            <Route path="/following" element={<FollowingPage />} />
+            <Route path="/following/:userId" element={<FollowingPage />} />
+            <Route path="/followers" element={<FollowersPage />} />
+            <Route path="/followers/:userId" element={<FollowersPage />} />
+            <Route path="/blocked" element={<BlockedUsersPage />} />
+            {/* Discovery / Notifications pages */}
+            <Route path="/feed" element={<DiscoveryFeedPage />} />
+            <Route path="/search" element={<SearchHubPage />} />
+            <Route path="/trending" element={<TrendingChartsPage />} />
           </Routes>
 
           <PulsifyPlayerBar />
