99cf152deb95024378c2726bc6fbcfd9e5770ac0
Omar Nasser <omarnasser2003@hotmail.com>
2026-04-22 09:26:36 +0200

    feat(navbar): finalize SoundCloud UI parity with solid icons and new dropdowns

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { PulsifyAuthVaultProvider } from "./store/PulsifyAuthVault.jsx";
 import { AuthProvider } from "@/contexts/AuthContext";
 import "./index.css";
+import "./css/navbar-soundcloud.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
